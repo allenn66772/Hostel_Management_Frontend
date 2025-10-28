29b1abc57e35c9c77897b98b344f2fb2395c58e2
allenn66772 <allenn4124@gmail.com>
2025-10-28 22:06:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      
+      <div className="h-screen flex items-center justify-center bg-green-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind is Working 🎉
+      </h1>
+    </div>
+      <h1 className='text-green-600'>bsbvibvius</h1>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    
+    
+    
+    
+    </>
   )
 }
 
