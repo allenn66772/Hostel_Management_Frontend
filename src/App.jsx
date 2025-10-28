20faf6a1d74aea03ebc701d11a9bbe738a5e2b2f
allenn66772 <allenn4124@gmail.com>
2025-10-28 22:31:12 +0530

    day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
 import './App.css'
+import Header from './components/Header'
+import UserLogin from './login/UserLogin'
+import AddRoom from './pages/Addroom'
+import Home from './pages/Home'
 
 function App() {
  
 
   return (
     <>
-      <div className="h-screen flex items-center justify-center bg-green-900">
-      <h1 className="text-4xl font-bold text-green-400">
-        Tailwind is Working 🎉
-      </h1>
-    </div>
-      <h1 className='text-green-600'>bsbvibvius</h1>
+     <Header/>
+     {/* <Home/> */}
+     {/* <AddRoom/> */}
+     <UserLogin/>
     </>
   )
 }
