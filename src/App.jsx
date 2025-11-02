b34c91b3ad5e55dc45c694278b87095e446834c7
allenn66772 <allenn4124@gmail.com>
2025-11-02 14:32:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Header from "./components/Header";
 import Staff_Login from "./login/Staff_Login";
 import UserLogin from "./login/UserLogin";
 import AddRoom from "./pages/Addroom";
-import ComplaintDisplay from "./pages/ComplaintPage";
+import Allroom from "./pages/Allroom"
+
 import FeesPage from "./pages/Fees";
 import Home from "./pages/Home";
 import Adminlogin from "./login/Adminlogin";
@@ -12,6 +13,9 @@ import Adminregister from "./login/Adminregister";
 import Userregister from "./login/Userregister";
 import ChooseLogin from "./login/ChooseLogin";
 import StaffRegister from "./login/StaffRegister";
+import Adminhome from "./pages/Adminhome";
+import ComplaintPage from "./pages/ComplaintPage"
+import Viewmaintenence from "./pages/Viewmaintenence"
 // import Adminhome from './pages/Adminhome'
 
 function App() {
@@ -20,14 +24,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="adreg" element={<Adminregister />} />
+        <Route path="adminhome" element={<Adminhome/>}/>
         <Route path="adlog" element={<Adminlogin />} />
         <Route path="userreg" element={<Userregister />} />
         <Route path="userlog" element={<UserLogin />} />
         <Route path="staffreg" element={<StaffRegister />} />
         <Route path="stafflog" element={<Staff_Login />} />
         <Route path="addroom" element={<AddRoom />} />
+        <Route path="complaints"element={<ComplaintPage/>}/>
+        <Route path="viewmain" element={<Viewmaintenence/>}/>
+        <Route path="allroom" element={<Allroom/>}/>
         <Route path="fees" element={<FeesPage />} />
-        <Route path="complaintpage" element={<ComplaintDisplay />} />
+       
         <Route path="choose" element={<ChooseLogin />} />
         {/* <Route path='adminHome' element={<Adminhome/>}/> */}
       </Routes>
